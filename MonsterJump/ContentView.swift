c46188787f258bb2c9650730086e51f822f8d103
Terry Kuo <terrykuo501@gmail.com>
2022-05-04 13:57:05 +0800

    Initial Commit

---
+++ MonsterJump/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MonsterJump
+//
+//  Created by Terry Kuo on 2022/5/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MonsterJump/MonsterJumpApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonsterJumpApp.swift
+//  MonsterJump
+//
+//  Created by Terry Kuo on 2022/5/4.
+//
+
+import SwiftUI
+
+@main
+struct MonsterJumpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
